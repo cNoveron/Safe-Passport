be47025fee174088a35ce07b6e3e5a8bcda8ea4d
carlosescom <carlos.escom.ipn@gmail.com>
2018-09-23 07:18:28 -0500

    Added functions to map a Pattern to one's own address or a third party's address.

--- contracts/address_to_Pattern.sol
+++ contracts/address_to_Pattern.sol
@@ -8,4 +8,11 @@ contract address_to_Pattern {
     mapping (Pattern => address) address_from_Pattern;
     // 1 of many to =1 of many
     
+    function map_sender_address_to_Pattern(uint Pattern) public {
+        Pattern_from_address[msg.sender] = Pattern;
+    }
+    
+    function map_address_to_Pattern(address address_of_third_party, uint Pattern) public {
+        Pattern_from_address[address_of_third_party] = Pattern;
+    }
 }
